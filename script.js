3b5b9641b6875725fd116dc8e8133b9eeb8d2a11
UJJWAL TOMAR <102405945+itsAditiSaraswat@users.noreply.github.com>
2022-12-20 16:40:06 +0530

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 var token = config.MY_API_TOKEN;
 
-document.getElementById("frame").src = `https://webchat.botframework.com/embed/glagroot-chatbott-bot?s=${token}`;
+document.getElementById("frame").src = `https://webchat.botframework.com/embed/MyInstance-001-bot?s=${token}`;
 document.getElementById("frame").style.backgroundColor = 'red';
