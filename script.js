1aa6263f04048f6f967f53b24e98739527e8d618
UJJWAL TOMAR <102405945+itsAditiSaraswat@users.noreply.github.com>
2022-12-20 13:53:52 +0530

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 var token = config.MY_API_TOKEN;
 
-document.getElementById("iframe").src = `https://webchat.botframework.com/embed/au-chatbot-bot?s=${token}`;
+document.getElementById("frame").src = `https://webchat.botframework.com/embed/glagroot-chatbott-bot?s=${token}`;
+document.getElementById("frame").style.backgroundColor = 'red';
